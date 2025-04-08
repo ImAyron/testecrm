9a503070e91b22245290054c56ffc8dcb90d383a
Ayron Luigi de paiva <49703457+ImAyron@users.noreply.github.com>
2025-04-08 14:27:22 -0300

    configurate routes

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FormSubmissionController;
+use App\Http\Controllers\LeadController;
 
 Route::get('/submissions', [FormSubmissionController::class, 'index'])->name('submissions.index');
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function() {
+    Route::get('leads', [LeadController::class, 'index'])->name('leads.index');
+    Route::get('leads/export', [LeadController::class, 'export'])->name('leads.export');
+    Route::delete('leads/{id}', [LeadController::class, 'destroy'])->name('leads.destroy');
+    Route::get('leads/{id}', [LeadController::class, 'show'])->name('leads.show');
+});
 
 Route::middleware([
     'auth:sanctum',
